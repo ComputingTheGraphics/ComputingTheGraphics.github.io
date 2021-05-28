1ae899895b6f506558d3f3f634b3fd7c42234147
Hannah Bollar <hanbollar@gmail.com>
2021-05-28 01:10:55 -0400

    update - allowing for saved contributors github information

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,7 @@
+# CONTRIBUTORS
+
+Here is a listing of the github contributors we've had in the past. Theyre ordered by date of earliest change.
+
+==========
+
+* [hanbollar](https://github.com/hanbollar)
--- prepareForSubmission.py
+++ prepareForSubmission.py
@@ -3,46 +3,47 @@
 import os
 from collections import OrderedDict
 
-def updatePostsMenu():
-    pythonfile = __file__
+pythonfile = __file__
 
+def updatePostsMenu():
     # if the file is present in current directory,
     # then no need to specify the whole location
     fullpath = os.path.abspath(pythonfile)
     directoryOfFile = os.path.dirname(fullpath)
-    print("Path of the file..", os.path.abspath(pythonfile))
 
     postsMenu = OrderedDict()
+    
+    print('checking from _order.txt files')
 
     for root, dirs, files in os.walk(r'{}'.format(directoryOfFile)):
         if '/posts' not in root:
             continue
         
-        print('root is: {}'.format(root))
-        print('-- dirs is : {}'.format(dirs))
-        print('-- files is : {}'.format(files))
-
         index = (root[::-1]).index('/')
         currDir = root[-index:]
+        orderFilepath = root + '/_order.txt'
+        print('--- {}'.format(orderFilepath))
         if currDir == 'posts':
-            print('------ A')
             # top level posts folder
-            order = getOrderFromTxt(root + '/_order.txt')
-            confirmOrderIsValid(order, dirs, root + '/_order.txt')
+            order = getOrderFromTxt(orderFilepath)
+            confirmOrderIsValid(order, dirs, orderFilepath)
+            for name in order:
+                postsMenu[name] = []
         else:
-            print('------ B')
             # sub level in posts folder
-            print('root + /_order.txt is : {}'.format(root + '/_order.txt'))
-            order = getOrderFromTxt(root + '/_order.txt')
-            print('order is : {}'.format(order))
+            order = getOrderFromTxt(orderFilepath)
             for name in order:
                 postsMenu[currDir] = \
                     [name] if currDir not in postsMenu \
                     else postsMenu[currDir] + [name]
-            print('---- postsMenu[{}] = {}'.format(currDir, postsMenu[currDir]))
+
+    print('\nFINAL POSTS MENU:')
+    for key in postsMenu:
+        print('{}'.format(key))
+        for post in postsMenu[key]:
+            print('\t{}'.format(post))
 
 def getOrderFromTxt(filepath):
-    print('getting order from _order.txt')
     with open(filepath) as f:
         contents = f.read()
         contentsList = contents.split("\n")
@@ -50,7 +51,6 @@ def getOrderFromTxt(filepath):
         return [item for item in contentsList if item != '']
 
 def confirmOrderIsValid(orderedNames, names, orderFile):
-    print('confirm _order.txt is valid for directory')
     error = '''\
 ---- in file: {}
 ---- _order.txt does not have the same names as those found in the directory.
@@ -63,16 +63,72 @@ def confirmOrderIsValid(orderedNames, names, orderFile):
             if name not in names:
                 raise ValueError(error)
 
-def addUserToContributersFile():
-    # TODO - grab github username from local command line
-    print('add user to contributers file')
+def addUserToContributorsFile():
+    print('''\
+We want to make sure all individuals are noted as contributors for computingthegraphics.com!
+
+Are you already noted as a contributor? Do you not want your contributions to be known :( ?
+You can skip this process.
+''')
+    valid = False
+    while not valid:
+        response = raw_input('>>>> WOULD YOU LIKE TO SKIP? TYPE ONE OF THE FOLLOWING (Y/N) : ')
+        if response in ('Y', 'y', 'N', 'n', 'yes', 'YES', 'no', 'NO'):
+            valid = True
+
+    if response in ('Y', 'y', 'yes', 'YES'):
+        return
+
+    print('''\n\
+You are added to the larger contributors file by your @githubHandle. Please note it below (case sensitive):''')
+    
+    valid = False
+    while not valid:
+        gitHandle = raw_input('>>>> @')
+        response = raw_input('>>>> IS THIS THE HANDLE YOU WISH TO SAVE @{} (Y/N) : '.format(gitHandle))
+        valid = response in ('Y', 'y', 'yes', 'YES')
+   
+    with open('CONTRIBUTORS.md', 'r+') as f:
+        contents = f.read()
+        contentsList = contents.split("\n")
+        try:
+            # Remove last empty line
+            finalContentsList = contentsList[:-1]
+        except:
+            # This will only be thrown if the file was missing for some reason and auto created.
+            pass
+        if gitHandle in finalContentsList:
+            print('''\
+You were already noted as a contributor. Thanks for the engagement!!!
+
+IF YOU THINK THIS IS A MISTAKE, NOTE IT AS AN ISSUE ON THE REPO OR IN THE DISCUSSION FORUM''')
+        else:
+            f.write('* [{}](https://github.com/{})\n'.format(gitHandle, gitHandle))
+            print('''\
+Your handle has now been added to our CONTRIBUTORS.md file. Thanks for the engagement!!!''')
+            
+        
+    print('''\nTODOOOOOOOOOOOOOOO - add a way to confirm in the pull request that if the user changed the contributors.md file - that the username matches that of the name change''') 
+
 
 # Ready for submit:
 try:
+    print("python run : {}".format(os.path.abspath(pythonfile)))
+    print('''\n\
+=========================
+== UPDATING POSTS MENU ==
+=========================\n''')
     updatePostsMenu()
-    addUserToContributersFile()
-    print('SUBMISSION READY FOR EDITORS')
+    print('''\n\
+===================================
+== ADD USER TO CONTRIBUTORS FILE ==
+===================================\n''')
+    addUserToContributorsFile()
+    print('''\n\
+=====================================
+== SUBMISSION READY FOR EDITORS :D ==
+=====================================\n''')
 except Exception as e:
     print(e)
-    print('\n\n== Errored while running final submission tasks. Make sure you ...... etc etc. ==')
-    print('\n\n====== SUBMISSION NOT READY TO BE REVIEWED BY EDITORS ======\n\n')
+    print('\n\n== Errored while running final submission tasks. ==')
+    print('\n\n====== !!! SUBMISSION NOT READY TO BE REVIEWED BY EDITORS !!! ======\n\n')
